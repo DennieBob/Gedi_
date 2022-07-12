ef31b87ff6d0530a32b4195c33ea01802d043943
ibiso <ibisobobmanuel.ib@gmail.com>
2022-07-12 21:30:45 +0100

    new branch commit

--- questionaire/views.py
+++ questionaire/views.py
@@ -15,59 +15,29 @@ from django.http import JsonResponse
 from django.core import serializers
 User = get_user_model()
 
-# class PagesView(LoginRequiredMixin,TemplateView):
-#     pass
-
-
-# questionaire = PagesView.as_view(template_name="questionaire/index.html")
-
+ 
 def questionaire_home(request):
-    questions=serializers.serialize("json", Question.objects.all() )#.values()
+    questions=serializers.serialize("json", Question.objects.all() ) 
     context={"questions":questions}
-    # print('over her this prt is working')
     return render(request,"questionaire/home.html",context)
 
 def questionaire(request):
-    questions=serializers.serialize("json", Questionaire.objects.filter(pk=1) )#.values()               this
+    questions=serializers.serialize("json", Questionaire.objects.filter(pk=1) ) 
     context={"questions":questions}
-    # print('over her this prt is working')
     return render(request,"questionaire/index.html",context)
 
-def get_questionaire_by_id(request):
-    # questions=serializers.serialize("json", Questionaire.objects.filter(pk=1) )#.values()               this
-    # questions=serializers.serialize("json", Questionaire.objects.all() )#.values()               this
-    # questions=serializers.serialize("json", Question.objects.get(reporter=1) )# get only one
+def get_questionaire_by_id(request): 
     if Question.objects.filter(questionaire=2).count() ==0:
         questions=json.dumps([0]) 
-        # question=json.loads(serializers.serialize("json", Question.objects.filter(questionaire=2) ))# filter      and this
-        
-        # print(question[len(question)-1]['fields']['options'])
-        # print(len(question))
-        # print(len(questions)) 
-        return JsonResponse(questions,safe=False)
-        # return HttpResponse(questions)
-    
+        return JsonResponse(questions,safe=False) 
     else:
-        questions=serializers.serialize("json", Question.objects.filter(questionaire=2) )# filter      and this
-        # question=json.loads(serializers.serialize("json", Question.objects.filter(questionaire=2) ))# filter      and this
-
-        # print(question[len(question)-1]['fields']['options'])
-        # print(len(question))
-        # print(len(questions)) 
+        questions=serializers.serialize("json", Question.objects.filter(questionaire=2) )
         return JsonResponse(questions,safe=False)
-        # return HttpResponse(questions)
 
-# def new_user(request):
 def update_questionaire(request):
-    # print('hereklsfklskdfls')
     if request.method == 'POST':
-        # fetch data from the ajax post
         body_unicode = request.body.decode('utf-8')
         received_data = json.loads(body_unicode) 
-        # print(received_data["questionaire_id"])
-        # print(received_data[0]['nextRef'])
-        # print(received_data[0]['groupText'])
-        # print(received_data[1])
         Question.objects.all().delete()
         for i in range(len(received_data)):
             obj=Question(
@@ -84,21 +54,10 @@ def update_questionaire(request):
                 isEnd=received_data[i]['isEnd'],
                 endNote= received_data[i]['endNote'],
             )
-            obj.save()
-        # save to the question but reference the questionaire id
-        # # update the user object
-        # u = User.objects.create(email=person['email'], username=person['username'],
-        #                         password=person['password'], first_name=person['first_name'],
-        #                         last_name=person['last_name'], phone=person['phone'],
-        #                         group=person['group'], permission=person['permission'])
-        # u.save()
-
-        # # pack response:
+            obj.save() 
         response = dumps({
             'status': 'ok',
-            # "user": u,
             "data": received_data,
-            # "person": int(person['id'])
         })
 
         return HttpResponse(response)
--- static/questionaire/app.js
+++ static/questionaire/app.js
@@ -2,13 +2,17 @@ const app = Vue.createApp({
     
     // template:`<h1>{{firstName}}</h1>`,
     data() {
-        return {
+        return { 
+           optionTitle:'',
+           nextRefPlaceholder:'',
             errors:{
+              errs:0,
               title:'',
               optionType:'',
               _type:'',
               multiselect:'',
               groupText:'',
+              nextRef:'',
               endNote:''
             },
             questionDB: [],
@@ -16,22 +20,19 @@ const app = Vue.createApp({
             isEmpty:[1],
             currentLastId:0,
             isInvalid:'  ',
-            //   constext: {},
             question: {
-        title: "",
-        optionType: "",
-        id: "",
-        type: "",
-        multiselect: "",
-        parentId: "",
-        options: [],
-        nextRef: '',
-        // nextRef: { title:''},
-        // nextRef: "",
-        groupText: "",
-        isEnd: "",
-        endNote: "",
-      },
+              title: "",
+              optionType: "",
+              id: "",
+              type: "",
+              multiselect: "",
+              parentId: "",
+              options: [],
+              nextRef: '', 
+              groupText: "",
+              isEnd: "",
+              endNote: "",
+            },
 
       empty_question: {
         title: "",
@@ -54,35 +55,37 @@ const app = Vue.createApp({
       editMode: false,
     };
   },
+  // watch: {
+  //   // whenever question changes, this function will run
+  //   question:function (k){
+  //     k.groupText= 'test'
+  //     console.log(this.question.groupText) 
+  //   }
+  // },
   beforeMount(){
     this.getData()
  },
   methods: {
       getData: async function () {
       console.log('fetched')
-      // this.questionDB=[]
       const res = await fetch("/questionaire/get").then(
         (response) => {
           return response.json(); 
         }
       );
       let data = await res;
-      // console.log(data)
       data = data.replaceAll("'false'",false)
       data = data.replaceAll("'true'",true)
-      // console.log(data)
 
       data = JSON.parse(data);
 
       if (data[0] !=0){
         this.isEmpty=[1]
-        // console.log(this.isEmpty)
         const arr =[] 
         let jsonOptions  
         data.map(el=>{ 
           jsonOptions=JSON.parse(el.fields.options.replaceAll("'",'"')) 
           let k=el.fields.nextRef.replaceAll('"','') 
-          // console.log(jsonOptions)
           let item = {
               title:el.fields.title,
               optionType:el.fields.optionType,
@@ -97,8 +100,7 @@ const app = Vue.createApp({
               endNote:el.fields.endNote,
           }
         arr.push(item)
-        }) 
-        console.log(arr)  
+        })  
         arr.sort((a,b)=>a.id-b.id) 
         this.questionDB=arr
         
@@ -107,33 +109,18 @@ const app = Vue.createApp({
       }
     }, 
     upDateDBNow: async function () { 
-        const model  = this.questionDB 
-        // console.log('model')
-        // console.log(this.questionDB)
-        // console.log(model.length)
+        const model  = this.questionDB  
         let modelled =[]
-        model.map(el=>{
-            // let optionsMapped  
-            // el.options.length>0?el.options.map(elm=>{
-            //     console.log('here')
-            //     optionsMapped.push({
-            //         ...elm,
-            //         // isEnd:elm.isEnd===true?'true':'false'
-            //     })
-            // }):optionsMapped=''
-            // console.log(optionsMapped==el.options)
-            // console.log(el.options)
-            // console.log(typeof(optionsMapped))
+        model.map(el=>{ 
             modelled.push({
             "groupText": el.groupText,
             "isEnd": el.isEnd,
             "multiselect": el.multiselect,
             "nextRef": el.nextRef,
-            "optionType": el.optionType,
-            // "options": optionsMapped,    //change and it worked
-            "options":  el.options,    //change and it worked
+            "optionType": el.optionType, 
+            "options":  el.options,     
             "parentId": el.parentId,
-            "reference": el.parentId,  //change
+            "reference": el.parentId,  
             "theId": el.id,
             "theType": el.type,
             "title": el.title,
@@ -141,15 +128,11 @@ const app = Vue.createApp({
             })
         }) 
         const csrftoken = Cookies.get('csrftoken');
-        console.log('hererererere')
         let payload = JSON.stringify(modelled)
-        // console.log(payload)
         payload = payload.replaceAll(false,'"false"')
         payload = payload.replaceAll(true,'"true"')
-        console.log('herererer')
-        // console.log(payload)
         fetch(`/questionaire/update/`, {
-            method: 'POST', // or 'PUT'
+            method: 'POST',  
             headers: {
               'X-CSRFToken': csrftoken,
               "x-Requested-With": "XMLHttpRequest",
@@ -165,84 +148,64 @@ const app = Vue.createApp({
           .catch((error) => {
             console.log('Error:', error);
           });
-    // }
-    // this.getData()
-      }, 
-    upDateDB(){ 
 
-      // this.handleInput()
-      // this.questionDB.map(el=>{
-      //   if(el.id=='') {
-      //     console.log('none')
-      //   }else if(el.id>this.currentLastId) this.currentLastId=el.id
-      // })
-      // this.questionDB.map.length>1&&this.questionDB.map(el=>{if(el.id>this.currentLastId) this.currentLastId=el.id})
-      // console.log(this.currentLastId) 
-            if(!this.editMode && this.questionDB.length===0){ 
-                console.log('new')
+      }, 
+      handleInput(){
+        this.errors.errs=0
+        const isError = ' form-control is-invalid required'
+        const {title,optionType,type:_type,multiselect,groupText,endNote,nextRef} = this.question
+  
+        if(!title){this.errors.title+=isError;this.errors.errs=1}else{this.errors.title=''}
+        if(!optionType){this.errors.optionType+=isError;this.errors.errs=1}else{this.errors.optionType=''}
+        if(!_type){this.errors._type+=isError;this.errors.errs=1}else{this.errors._type=''}
+        if(this.question.optionType=="Predefined")if(!multiselect){this.errors.multiselect+=isError;this.errors.errs=1}else{this.errors.multiselect=''}
+        if(this.question.optionType=="Custom")if(!nextRef){this.errors.nextRef+=isError;this.errors.errs=1}else{this.errors.nextRef=''}
+      },
+    upDateDB(){ 
+      this.handleInput() 
+      if(this.errors.errs==0){ 
+        if(!this.editMode && this.questionDB.length===0){ 
                 this.questionDB = [...this.questionDB,{...this.question,id:this.questionDB.length+1}]
                 this.currentLastId+=1
                 let obj=0
                 this.questionDB[obj].options.length>0&&this.questionDB[obj].options.map((item,index)=>{
+                  if(item.next_question!=''){
                     this.questionDB[obj].options[index].id=this.questionDB.length+1
                     this.questionDB.push({...this.empty_question,title:item.next_question,options:[],parentId:obj,id:this.questionDB.length+1})
-                    // this.currentLastId+=1 
-                })
+                  }
+                  })
                 if(this.question.nextRef !==''){  
-                // this.questionDB[obj].nextRef =this.questionDB.length+1
                 this.questionDB.push({...this.empty_question,title:this.question.nextRef ,options:[],parentId:obj,id:this.questionDB.length+1})
               }
-              // if(this.question.nextRef.title!==''){  
-              //   this.questionDB[obj].nextRef.id=this.questionDB.length+1
-              //   this.questionDB.push({...this.empty_question,title:this.question.nextRef.title,options:[],parentId:obj,id:this.questionDB.length+1})
-              // }
-                console.log(this.question.nextRef!=='')
 
-            }else{ 
-                this.questionDB.map((each,index)=>{
+            }else{
+              this.questionDB.map((each,index)=>{
                     if(each.id===this.question.id) {
-                        // get this updated object index
                         let obj
                         this.questionDB.filter((el,index)=>{if(el.id==each.id)obj=index})
-                        // update the object by index
-                        this.questionDB[obj]= {...this.questionDB[obj],...this.question}
-
-
-                        // if(this.questionDB[obj].nextRef.title!==''){
-                        //   this.questionDB[obj].nextRef.id=this.questionDB.length+1
-                        //   this.questionDB.push({...this.empty_question,title:this.question.nextRef.title,options:[],parentId:obj,id:this.questionDB.length+1})
-                        // }
-                        // this.questionDB.map(eels=>{
-                        //   console.log(eels.title==this.questionDB[obj].nextRef)
-                          // if(eels.title!==this.questionDB[obj].nextRef){ 
+                        this.questionDB[obj]= {...this.questionDB[obj],...this.question} 
                             tocheck=this.questionDB.some(element=>element.title == this.questionDB[obj].nextRef)
-                            console.log(tocheck)
-                            console.log(tocheck==false)
-                            if(this.questionDB[obj].nextRef!=='' && tocheck==false){
-                            this.questionDB.push({...this.empty_question,title:this.question.nextRef,options:[],parentId:obj,id:this.questionDB.length+1})
-                          }
-                      //  }})
-                        // loop through references of this object and update
-                        this.question.options.map((item,index)=>{
-                          let check = this.questionDB.some(element=>element.id == item.id)
-                          //  
-                          // 
-                          // console.log(item.id==check)
-                          // console.log(this.question.id == item.parentId)
-                          // console.log(item.id, check)
-                          // console.log(this.question.id, item.parentId)
+                             
+                            if(this.questionDB[obj].nextRef!=='' && tocheck==false){ 
+                              if(this.nextRefPlaceholder==''){
+                              this.questionDB.push({...this.empty_question,title:this.question.nextRef,options:[],parentId:obj,id:this.questionDB.length+1})
+                              }else{
+                                let ind=this.questionDB.findIndex(x=>x.title==this.nextRefPlaceholder)
+                                this.questionDB[ind].title = this.question.nextRef
+                              }
+                            } 
+                         this.question.options.map((item,index)=>{
+                          let check = this.questionDB.some(element=>element.id == item.id) 
                           if(!item.id){
+                            //create new
+                            if(item.next_question!=''){
                             this.questionDB[obj].options[index].id=this.questionDB.length+1
-                            // add id to new questions and make them their own questions
                             this.questionDB.push({...this.empty_question,title:item.next_question,options:[],parentId:obj,id:this.questionDB.length+1})
-                            // this.questionDB.push({...this.empty_question,title:item.next_question,options:[],parentId:obj,id:this.questionDB.length+1})
-                          //  this.currentLastId+=1
-                            
+                            }
                           }else{
-                                // update option name in its own question
+                                // update old
                                 let objIndex
                                 this.questionDB.filter((el,index)=>{if(el.id==item.id)objIndex=index})
-                                // console.log(objIndex)
                                 this.questionDB[objIndex]={...this.questionDB[objIndex],title:this.question.options[index].next_question}
                             }
                         })
@@ -258,22 +221,11 @@ const app = Vue.createApp({
         console.log(this.questionDB)
 
         this.question = {...this.empty_question}
-        this.editMode=false
-
-
-    },
-    handleInput(){
-      let errs=0
-      const isError = ' form-control is-invalid required'
-      const {title,optionType,type:_type,multiselect,groupText,endNote} = this.question
-
-      if(!title){this.errors.title+=isError;errs=1}else{this.errors.title=''}
-      if(!optionType){this.errors.optionType+=isError;errs=1}else{this.errors.optionType=''}
-      if(!_type){this.errors._type+=isError;errs=1}else{this.errors._type=''}
-      if(this.question.optionType=="Predefined")if(!multiselect){this.errors.multiselect+=isError;errs=1}else{this.errors.multiselect=''}
-      // if(!groupText){this.errors.groupText+=isError;errs=1}else{this.errors.groupText=''}
-      // if(!endNote){this.errors.endNote+=isError;errs=1}else{this.errors.endNote=''}
-      errs==0&&this.upDateDB()
+        // this.optionTitle = null
+        this.optionTitle = {...this.empty_question}
+        this.nextRefPlaceholder==''
+        this.editMode=false 
+          }
     },
     switchRadio() {
       this.question.multiselect.status = !this.question.multiselect.status;
@@ -289,30 +241,16 @@ const app = Vue.createApp({
         this.question.options[index - 1].next_question;
     },
     add() {
-      // let errors=[]
-      // this.question.title
-      // this.question.optionType
-      // this.question.type
-      // this.question.multiselect
-      // this.question.reference
-      // this.question.options
-      // this.question.nextRef
-      // this.question.groupText
-      // this.question.isEnd
-
       this.question.options.push({
         option: "",
         next_question: "",
         isEnd: "",
-        // id:this.questionDB.length+this.question.options.length
       });
     },
     change(){
-        console.log('herel')
         window.location.href = "http://example.com"
     },
     editOptions() {
-      //fix updating later
       if (this.clicked === false) {
         this.indx = this.question.selectPredefinedOptions.length - 1;
       } else {
@@ -331,39 +269,47 @@ const app = Vue.createApp({
       }
       return readString;
     },
-    evtChange(event){ 
-      console.log('here')
-      console.log(event.target.value)
+    evtChange2(event){   
+      this.errors.errs=0  
       const check = this.questionDB.filter((x) => {
-          // console.log(x)
-          if (x.id == event.target.value) {
+          if (x.id == this.optionTitle.id) {
             return x;
           }
         }); 
-      console.log(check[0]) 
         this.question = check[0]; 
+        if(check[0].nextRef!='') {this.nextRefPlaceholder=check[0].nextRef} 
         this.editMode = true; 
+
+      },
+    evtChange1(event){   
+      this.errors.errs=0
+      const check = this.questionDB.filter((x) => {
+        if (x.id == event.target.value) {
+          return x;
+        }
+      }); 
+      this.question = check[0]; 
+      if(check[0].nextRef!='') {this.nextRefPlaceholder=check[0].nextRef} 
+      this.editMode = true; 
     },
-    editQuestion(e) { 
-      const even = {target:{value:event.target.id}}
-    
-      // const check = this.questionDB.filter((x) => { 
-      //   if (x.id == e.target.id) {
-      //     return x;
-      //   }
-      // }); 
-      // this.question = check[0]; 
-      // this.editMode = true;
-      this.evtChange(even)
+    editQuestion(e) {  
+      this.errors.errs=0
+      const check = this.questionDB.filter((x) => {
+        if (x.id == e.target.id) {
+          return x;
+        }
+      }); 
+      this.question = check[0]; 
+      this.optionTitle =  check[0]
+      if(check[0].nextRef!='') {this.nextRefPlaceholder=check[0].nextRef} 
+      this.editMode = true;   
     },
     deleteQuestion(e) {
       this.questionDB =
         this.questionDB.length > 0 &&
         this.questionDB.filter((x) => x.id != e.target.id);
       this.question = { ...this.empty_question };
-      // console.log(this.questionDB.length)
       if (this.questionDB.length>0){this.isEmpty=[1]}else{this.isEmpty=[0]}
-      // console.log(this.isEmpty)
 
       this.upDateDBNow()
 
